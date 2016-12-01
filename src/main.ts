f4d7e5ccd2e4a1205ca643f856a9901a22e01c1a
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-12-01 20:40:36 +0100

    1.0e import ./app/index -> boostrap ng-app="myapp"

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import { app } from './app/_app.module';
 // import polyfills
 import './polyfills.ts';
 import './vendor.ts';
+
+// import for old Angular module bootstrap ng-app="myapp"
+import './app/index';
