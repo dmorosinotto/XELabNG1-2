91a2eed6e76e5191a4ea69df14bf72407b793f69
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-12-01 17:54:15 +0100

    0.4 service factory share logic

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 var app = angular.module("myapp",[]);
 
-app.controller("askCtrl", function(){
+app.controller("askCtrl", ["nameSvc", function(svc) {
     var vm=this;
-    //vm.name = "Daniele";
+    vm.name = svc.name;
 
     vm.show = function() {
-        if (vm.name) {
-            alert("Hi, " + vm.name);
+        svc.show(vm.name);
+    }
+}]);
+
+app.factory("nameSvc", function() {
+    var _name = "";
+
+    function set_Show(val) {
+        _name = val;
+        if (_name) {
+            alert("Hi, " + _name);
         } else alert("WRITE YOUR NAME");
     }
+
+    return {
+        name: _name,
+        show: set_Show
+    };
 });
