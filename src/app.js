cacfaf391679256aa9a2ea03126bb62dd925a542
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-12-01 20:36:41 +0100

    1.0d keysDir -> app/directives (FS)

--- src/app.js
+++ src/app.js
@@ -4,26 +4,3 @@
 
 
 
-app.directive("keys", function(){
-    //DDO = Directive Definition Object - read more https://docs.angularjs.org/api/ng/service/$compile
-    var DDO = {
-        restrict: "A",
-        scope: {
-            onEsc: "&"
-        },
-        controller: function($scope, $element, $attrs) {
-            $element.on("keyup", onKeyUp);
-
-            function onKeyUp(ev) {
-                //console.log(ev);
-                if (ev.which==27) //ESC 
-                    $scope.$apply( $scope.onEsc({$event: ev}) );
-            }
-
-            $scope.$on("$destroy", function() { //LIFE-CYCLE $onDestroy
-                $element.off("keyup", onKeyUp); //PREVENT MEMORY LEAK!!
-            });
-        }
-    }
-    return DDO;
-})
