7fd8f217e39998cc903fccaeb1884e64d4d7ecb5
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-12-02 12:38:48 +0100

    1.9 TS routesConfig definition using ngRoute - add function routesConfig to _app.module .config() - point route -> template: <cmp></cmp> - optional use resolve: {"data" : ...} + $resolve to pass data into cmp @Input

--- src/app/_app.module.ts
+++ src/app/_app.module.ts
@@ -1,6 +1,20 @@
 import * as angular from "angular";
+import * as ngRoute from "angular-route";
 
 // DECLARE NG1 MODULE AND EVENTUALLY DEFINE constants + config / run
-export let app = angular.module("myapp", [])
+export let app = angular.module("myapp", [ngRoute])
     //.constant("WAIT",3000)
+    .config(routesConfig)
 ;
+
+// LEGACY NG1 angular-route CONFIGURATION
+routesConfig.$inject = ["$routeProvider"];
+export function routesConfig($routeProvider: angular.route.IRouteProvider){
+  $routeProvider
+    .when("/welcome", { template: "<welcome-cmp></welcome-cmp>" })
+    .otherwise("welcome");
+  //IF YOU NEED TO PASS DATA IN THE ROUTE USE 
+  //, resolve: { "data": ()=>...any|ng.IPromise }
+  //AND PASS IT TO COMPONENT @Input("=") r;
+  //template: "<cmp r='$resolve'></cmp>" 
+}
