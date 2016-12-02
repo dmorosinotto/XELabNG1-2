39969835246f129b8f7e012bb1d6e5e5452d03b0
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-12-02 10:24:25 +0100

    2.0 ng-metadata bootstrap @NgModule - remove ng-app="myapp" from index.html - imports old/legacy "myapp" ng1 module name into @NgModule

---
+++ src/app/feedback/_feedback.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from 'ng-metadata/core';
+
+import myapp from '../index'; //OLD-LEGACY NG1 APP module.name
+
+
+
+
+@NgModule({
+  imports: [
+      myapp
+  ],
+  declarations: [
+  ], 
+  providers: [
+  ]
+})
+export class FeedbackModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { app } from './app/_app.module';
 // import polyfills
 import './polyfills.ts';
 import './vendor.ts';
 
-// import for old Angular module bootstrap ng-app="myapp"
-import './app/index';
+
+import { platformBrowserDynamic } from 'ng-metadata/platform-browser-dynamic';
+import { FeedbackModule } from './app/feedback/_feedback.module';
+
+platformBrowserDynamic().bootstrapModule(FeedbackModule);
