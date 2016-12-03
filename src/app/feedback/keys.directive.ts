34d368b773923f7d82d83f8c1ef64ef5a2f801df
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-12-03 16:10:43 +0100

    E0 - Add handle for ENTER key to confirm input data without press Ok! - simply add a new @Output onEnter to keys.directive - and in the template welcomeCmp.html call same event handler fo OK

--- src/app/feedback/keys.directive.ts
+++ src/app/feedback/keys.directive.ts
@@ -10,8 +10,11 @@ export class KeysDirective {
     onKeyUp(ev: KeyboardEvent) {
         if (ev.which==27) //ESC 
             this.onEsc.emit(ev);
+        if (ev.which==13) //ENTER 
+            this.onEnter.emit(ev);
     }
 
     @Output() public onEsc = new EventEmitter()
+    @Output() public onEnter = new EventEmitter()
 }
 
